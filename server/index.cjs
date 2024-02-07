93075e287eeea400e82a730848c773f84bffab37
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-02-07 13:13:35 +0530

    small update

--- server/index.cjs
+++ server/index.cjs
@@ -1,32 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // const dotenv = require("dotenv");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // const cors = require('cors')
-var https = require('https');
+var https = require("https");
 
- var fs = require('fs');
- mongoose.set('strictQuery', false);
- 
- 
- const url = 'mongodb://0.0.0.0:27017/books';
+var fs = require("fs");
+mongoose.set("strictQuery", false);
 
- mongoose.connect(url, (err,client)=>{
-    if(!err) {
-        console.log("successful connection with the server");  
-    }
-    else
-        console.log("Error in the connectivity");
-})
- 
+const url = "mongodb://0.0.0.0:27017/books";
+
+mongoose.connect(url, (err, client) => {
+  if (!err) {
+    console.log("successful connection with the server");
+  } else console.log("Error in the connectivity");
+});
 
 app.use(express.json());
 // app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const quotes = require("./routes/quotes.cjs")
-app.use("/quotes_api/Quotes", quotes)
+const quotes = require("./routes/quotes.cjs");
+app.use("/quotes_api/Quotes", quotes);
 
-
-app.listen(5022, () => console.log("server up and runing on port 5000!"));
+const port = 5023; // Change to any available port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
